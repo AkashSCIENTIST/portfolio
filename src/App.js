d9a2e662d136001fefe164ed722e10fca4b0ea24
Akash <spakash182@gmail.com>
2023-02-05 10:33:38 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Navbar from "./Navbar";
 import "./App.css";
 import Contacts from "./Contacts";
+import PageLoading from "./PageLoading";
+import { useEffect, useState } from "react";
 
 function App(){
+  const [loaded, isLoaded] = useState();
+  return <>
+  {!isLoaded &&<PageLoading/>}
+  {isLoaded && <Page isLoaded={isLoaded}/>}
+  </>
+}
+
+function Page({isLoaded}){
+  useEffect(() => {
+    isLoaded(true);
+  }, []);
   return <>
     <Navbar/>
     <Contacts/>
---
+++ src/PageLoading.js
@@ -0,0 +1,7 @@
+import loader from "./preloader.gif";
+
+function PageLoading(props) {
+    return <img src={loader} alt="Loading..."  className="loader"/>
+}
+
+export default PageLoading;
